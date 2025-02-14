a1335e0a72aaf31d212f576ef9bbf5f5966c5453
Art Anr_pi3 <artpi@anrpi3.local>
2025-02-13 20:49:32 -0600

    basic examples Arduino

--- arduino/sketches/arduino_starter_kit/blink_updated/blink_updated.ino
+++ arduino/sketches/arduino_starter_kit/blink_updated/blink_updated.ino
@@ -43,7 +43,7 @@ void loop() {
 
   Serial.println("  ... top of the loop ...");
   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(300);                      // wait for a second
+  delay(1000);                      // wait for a second
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
   delay(100);                      // wait for a second
 }
