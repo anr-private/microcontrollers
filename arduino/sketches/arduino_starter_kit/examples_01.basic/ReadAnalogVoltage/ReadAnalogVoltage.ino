67688d8c81bfa623057dc58ae536681952abdb20
Art Anr_pi3 <artpi@anrpi3.local>
2025-02-25 22:28:26 -0600

    arduino read analog volts using pot

--- arduino/sketches/arduino_starter_kit/examples_01.basic/ReadAnalogVoltage/ReadAnalogVoltage.ino
+++ arduino/sketches/arduino_starter_kit/examples_01.basic/ReadAnalogVoltage/ReadAnalogVoltage.ino
@@ -10,9 +10,21 @@
   This example code is in the public domain.
 
   https://www.arduino.cc/en/Tutorial/BuiltInExamples/ReadAnalogVoltage
+
+  Circuit:  UNO R3
+    10K pot
+    Low end = gnd
+    High end = 5v
+    Wiper = Analog pin A0
 */
 
-int prev = -1;
+int ANALOG_PIN = A0;
+
+// If the current and prev differ by this much (or less), ignore the value
+int MIN_DIFF = 4;
+
+// previous raw value from analog pin input
+int PREV_RAW = -1;
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -24,21 +36,34 @@ void setup() {
 // the loop routine runs over and over again forever:
 void loop() {
   
-  // read the input on analog pin 0:
-  int sensorValue = analogRead(A0);
-  
-  if (sensorValue == prev) {
+  // read the input on analog pin:
+  int raw_value = analogRead(ANALOG_PIN);
+
+  int diff = abs(raw_value - PREV_RAW);
+  ////PREV_RAW = raw_value;
+
+  ////Serial.print("diff="); Serial.print(diff, DEC); Serial.println();
+
+  // If the difference between current and previous sensor values is small,
+  // ignore the value and wait for a bigger difference.
+  if (diff <= MIN_DIFF) {
     delay(0.001);
     return; 
   }
-  prev = sensorValue;
+/*
+  if (raw_value == ANALOG_PIN) {
+    delay(0.001);
+    return; 
+  }*/
+  PREV_RAW = raw_value;
+
   
   // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  float voltage = sensorValue * (5.0 / 1023.0);
+  float voltage = raw_value * (5.0 / 1023.0);
 
   // print out the value you read:
   Serial.print(" raw=");
-  Serial.print(sensorValue);
+  Serial.print(raw_value);
   Serial.print(" V=");
   Serial.println(voltage);
 
