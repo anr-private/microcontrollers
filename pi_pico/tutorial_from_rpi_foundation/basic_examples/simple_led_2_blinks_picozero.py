8cfcdfd556659a385a0d2fc9b9ed4d399bf2c7ed
Art SHADOW <art@SHADOW>
2025-05-02 13:27:53 -0500

    pico tutorial

---
+++ pi_pico/tutorial_from_rpi_foundation/basic_examples/simple_led_2_blinks_picozero.py
@@ -0,0 +1,22 @@
+# simple test for picozero
+from picozero import pico_led
+import time
+
+def blink_led(secs):
+    pico_led.on()
+    time.sleep(secs)
+    pico_led.off()
+    time.sleep(secs)
+
+
+while True:
+    blink_led(0.4)
+    blink_led(0.4)
+    time.sleep(0.65)
+    blink_led(0.15)
+    blink_led(0.15)
+    
+    time.sleep(1.5)
+    
+### end ###
+
