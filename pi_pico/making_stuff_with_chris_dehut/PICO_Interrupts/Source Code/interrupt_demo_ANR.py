6c01c5e3d63ab7ed671861d3ecddd9c2335d2b3f
Art SHADOW <art@SHADOW>
2025-07-16 15:02:23 -0500

    pi pico demo: pushbutton using irq

--- pi_pico/making_stuff_with_chris_dehut/PICO_Interrupts/Source Code/interrupt_demo_ANR.py
+++ pi_pico/making_stuff_with_chris_dehut/PICO_Interrupts/Source Code/interrupt_demo_ANR.py
@@ -12,18 +12,22 @@
 #load libraries
 import machine
 import utime
+import _thread
+
+###  CONSTANTS  ##########################
+
+PUSHBUTTON_IRQ_TRIGGER = machine.Pin.IRQ_RISING | machine.Pin.IRQ_FALLING
 
 ### GLOBALS  #########################
 #Preset the STATE variable for the pushbutton
 pushbutton_state = 0
 
 # the button obj
-####@@@@pushbutton = None
 pushbutton = machine.Pin(15,machine.Pin.IN,machine.Pin.PULL_DOWN)
 
 # the indicator LED
 btn_state_led = machine.Pin(16,machine.Pin.OUT)  #Create an output pin for warning LED
-btn_state_led.value(0)                           #Set it to OFF
+###btn_state_led.value(0)                           #Set it to OFF
 
 
 def set_pushbutton_state(new_state=None):
@@ -47,7 +51,11 @@ def pushbutton_handler(pin):
     global pushbutton_state
     
     pushbutton.irq(handler=None)
-    
+    #r = pushbutton.irq(handler=None)
+    #print(f"@@@ irq(none) is {type(r)}  {r}")
+    #print(f"@@@ {dir(r)}")
+    ###print(f"@@@ irq(none) is 0x{r:04x}")
+
     pb_value = pushbutton.value()
     
     if 0:
@@ -64,11 +72,12 @@ def pushbutton_handler(pin):
     else:
         print(f"  BOUNCED: pb.val={pb_value}  pb-state={pushbutton_state}")
         
-    pushbutton.irq(handler=pushbutton_handler)
+    pushbutton.irq(trigger=PUSHBUTTON_IRQ_TRIGGER, handler=pushbutton_handler)
     
 
 #Setup the Interrupt Request Handling for pushbutton change of state
-pushbutton.irq(trigger=machine.Pin.IRQ_RISING | machine.Pin.IRQ_FALLING, handler=pushbutton_handler)
+###@@@pushbutton.irq(trigger=machine.Pin.IRQ_RISING | machine.Pin.IRQ_FALLING, handler=pushbutton_handler)
+pushbutton.irq(trigger=PUSHBUTTON_IRQ_TRIGGER, handler=pushbutton_handler)
 
 # Get the initial state
 set_pushbutton_state()
@@ -76,27 +85,12 @@ print("pushbutton State=", pushbutton_state)
 
 # A loop just to keep things running, but does nothing constructive
 print("--- start main loop ----")
+if 1:
+    print(f"Thread ")
+    
 while True:  #run an endless loop as the main loop
     utime.sleep(.0001)
     #Perform all other activites here
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-#######################################################################################
-#Setup the Interrupt Request Handling for pushbutton change of state
-pushbutton.irq(trigger=machine.Pin.IRQ_RISING, handler=pushbutton_handler)
-pushbutton.irq(trigger=machine.Pin.IRQ_FALLING, handler=pushbutton_handler)
-pushbutton.irq(trigger=machine.Pin.IRQ_RISING | machine.Pin.IRQ_FALLING, handler=pushbutton_handler)
-
-
+### end ###
+    
