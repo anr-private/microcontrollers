a6585b1431f2671ba554142c6cc952912b76f6ef
Art SHADOW <art@SHADOW>
2025-07-16 17:34:26 -0500

    pi pico LED simple test - DVOZVO Pico W boards internal LED gpio 25 does not work on any of the ones I bought

--- pi_pico/making_stuff_with_chris_dehut/PICO_Autorun/Source Code/LED.py
+++ pi_pico/making_stuff_with_chris_dehut/PICO_Autorun/Source Code/LED.py
@@ -1,9 +1,19 @@
+# LED.py  
+# blink GPIO 25 internal LED    ANR
+# Does not work with the 'clone' Pico W(?) brand DVOZVO boards
+
 import machine
 import utime
 
-LED = machine.Pin(25, machine.Pin.OUT) #Set pin 25 as output
+# GPIO 25 (no board pin) is 'internal led' - does not appear to work on the clone W boards I bought
+# board pin 3 is GND
+# board pin 20 is GPIO 15
+pin = 25
+pin = 15
+LED = machine.Pin(pin, machine.Pin.OUT) #Set pin 25 as output
 
 while True:
+    print("  blink the internal LED")
     LED.value(1) #Turn on LED
     utime.sleep(.5) 
     LED.value(0) #Turn off LED
