b020ae5a8f9926127935d79519dc8ce02b8cd80c
Art SHADOW <art@SHADOW>
2025-07-17 20:50:13 -0500

    pi pico PWM demo

--- pi_pico/making_stuff_with_chris_dehut/0030_PICO_PWM/Source Code/PWM-B0.py
+++ pi_pico/making_stuff_with_chris_dehut/0030_PICO_PWM/Source Code/PWM-B0.py
@@ -1,12 +1,14 @@
-# using PWM control
+# using PWM control     ANR 
+#
+# Sample duty cycles - manually selected
+# GPIO 2 (physical pin 4)  -> LED -> resistor 330  -> gnd  (physical pin 3)
 
-#load libraries
 import machine
 import utime
 
-#Create a PWM object called PWM_Sig
 PWM_Sig = machine.PWM(machine.Pin(2))  #use pin 4/GP2 for LED output
-PWM_Sig.freq(1000)  # 1000
+PWM_Sig.freq(1000)  # 1000 is good starting point; try higher and lower
+#PWM_Sig.freq(10)  # 1000 is good starting point; try higher and lower
 
 #65535 * 0.05 =  3276  #5% Duty Cycle
 #65535 * 0.25 = 16383  #25% Duty Cycle
@@ -14,12 +16,32 @@ PWM_Sig.freq(1000)  # 1000
 #65535 * 0.75 = 49151  #75% Duty Cycle
 #65535 * 1.00 = 65535  #100% Duty Cycle
 
-Duty_Cycle = round(65535 * .50)
-PWM_Sig.duty_u16(Duty_Cycle)
+#Duty_Cycle5 = round(65535 * .05)
+#Duty_Cycle25 = round(65535 * .25)
+#Duty_Cycle50 = round(65535 * .50)
+#Duty_Cycle75 = round(65535 * .75)
+#Duty_Cycle100 = round(65535 * 1.00)
 
-x = input("Hit enter to end") #Run the program till the user hits a key
+# percent duty cycle  0..100
+pct = 75
 
-PWM_Sig.duty_u16(1)
+duty_cycle = round (65535.0 * (pct / 100.0))
+
+print(f"Duty cycle  pct={pct}%  cycle_count={duty_cycle}     Use ^C to terminate this program.")
+PWM_Sig.duty_u16(duty_cycle)
+
+try:
+    while True:
+        utime.sleep(0.25)
+except KeyboardInterrupt:
+    print("   ended by ctrl-C")
+
+#x = input("Hit enter to end") #Run the program till the user hits a key
+###utime.sleep(2)
+print("  Cleanup...")
+###PWM_Sig.duty_u16(65000)
+PWM_Sig.duty_u16(10)
+utime.sleep(0.001) # not sure why, but this allows LED to go dark
 PWM_Sig.deinit()
 
 print("PROGRAM ENDED")
--- pi_pico/making_stuff_with_chris_dehut/0030_PICO_PWM/Source Code/PWM-B1.py
+++ pi_pico/making_stuff_with_chris_dehut/0030_PICO_PWM/Source Code/PWM-B1.py
@@ -1,23 +1,27 @@
-# using PWM control
+# using PWM control to fade an LED up/down   ANR
+#
+# GPIO 2 (physical pin 4)  -> LED -> resistor 330  -> gnd  (physical pin 3)
 
-#load libraries
 import machine
 import utime
 
-#Create a PWM object called PWM_Sig
+
 PWM_Sig = machine.PWM(machine.Pin(2))  #use pin 4/GP2 for LED output
 PWM_Sig.freq(1000)  # 1000
+# 10 Hz causes obvious flicker - it is worst at the lower duty cycles
+#PWM_Sig.freq(10)  # 1000
 
-print("Ready, Set, GO!")
+print("Main: begin")
 
+# fade up
 Duty_Cycle = 0     #range = 0 ~ 65535
 while Duty_Cycle <= 65535:
     Duty_Cycle += 1
     PWM_Sig.duty_u16(Duty_Cycle)
     utime.sleep(.0001)
 
-
-Duty_Cycle = 65531     #range = 0 ~ 65535
+# fade down
+Duty_Cycle = 65535
 while Duty_Cycle >= 0:
     Duty_Cycle -= 1
     PWM_Sig.duty_u16(Duty_Cycle)
