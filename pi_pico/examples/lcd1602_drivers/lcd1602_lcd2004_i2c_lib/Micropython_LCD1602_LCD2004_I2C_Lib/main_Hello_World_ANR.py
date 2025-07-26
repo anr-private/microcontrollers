2ecabb157c9a17fd6ad6fe63695bd697d48476f2
Art SHADOW <art@SHADOW>
2025-07-25 21:38:39 -0500

    LCD16021 PCF8754T driver and example

--- pi_pico/examples/lcd1602_drivers/lcd1602_lcd2004_i2c_lib/Micropython_LCD1602_LCD2004_I2C_Lib/main_Hello_World_ANR.py
+++ pi_pico/examples/lcd1602_drivers/lcd1602_lcd2004_i2c_lib/Micropython_LCD1602_LCD2004_I2C_Lib/main_Hello_World_ANR.py
@@ -9,13 +9,39 @@ scl_pin = 5
 sda_pin = 4
 lcd = LCD(SoftI2C(scl=Pin(scl_pin), sda=Pin(sda_pin), freq=100000))
 
+NUM_ROWS = 2
+#NUM_ROWS = 4
+
+print(f"HELLO WORLD TEST FOR PCF8754T style LCD displays.  num_rows={NUM_ROWS}")
+print(f"   Pins:  DATA={sda_pin}  CLOCK={scl_pin}  I2C-addr={'see-the-driver-code'}")
+
 try:
     print("Use ^C to terminate the program and perform cleanup on the LCD device.")
     while True:
-        lcd.puts("Hello, World!")
+        if NUM_ROWS == 2:
+            lcd.puts("Hello, World! ANR")
+        elif NUM_ROWS == 4:
+            lcd.puts("Hello World ANR", x=0, y=0)
+            lcd.puts("Hello World ANR row2", x=0, y=1)
+            lcd.puts("Hello World ANR row3", x=0, y=2)
+            lcd.puts("Hello World ANR row4", x=0, y=3)
+        else:
+            print(f"***ERROR*** Unexpected NUM_ROWS={NUM_ROWS}")
+            
         utime.sleep(1)
-        lcd.puts("      ")  # erases just the 'Hello'
-        #lcd.clear()     # erases whole screen
+        
+        if NUM_ROWS == 2:
+            lcd.puts("      ")  # erases just the 'Hello'
+            #lcd.clear()     # erases whole screen
+        elif NUM_ROWS == 4:
+            lcd.puts("      ", x=0, y=0)
+            lcd.puts("Hello ..... ANR row2", x=0, y=1)
+            lcd.puts("Hello World ... row3", x=0, y=2)
+            lcd.puts("Hello +++++ === row4", x=0, y=3)
+            #lcd.clear()
+        else:
+            print(f"***ERROR*** Unexpected NUM_ROWS={NUM_ROWS}")
+        
         utime.sleep(0.25)
 except (KeyboardInterrupt):
     lcd.clear()
