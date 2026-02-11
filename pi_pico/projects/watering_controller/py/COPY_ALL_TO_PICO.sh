c6aeb39cc6d38d007af6d9014238f3f306535013
Art SHADOW <art@SHADOW>
2026-02-10 21:26:41 -0600

    WIP release procedure for watering project

--- pi_pico/projects/watering_controller/py/COPY_ALL_TO_PICO.sh
+++ pi_pico/projects/watering_controller/py/COPY_ALL_TO_PICO.sh
@@ -5,27 +5,40 @@
 rm -rf anr_http/__pycache__
 
 #--- get rid of the old stuff ---
-
-echo 'CLEANUP: copy the cleanup program and run it'
-mpremote fs cp remove_old_watering_files.py  :/
-echo '   Run the cleanup'
-mpremote run remove_old_watering_files.py
-echo 'CLEANUP is done'
-echo '----------------'
+clean_up_old_dirs_and_files() {
+    echo 'CLEANUP: copy the cleanup program and run it'
+    mpremote fs cp remove_old_watering_files.py  :/
+    echo '   Run the cleanup'
+    mpremote run remove_old_watering_files.py
+    echo 'CLEANUP is done'
+    echo '----------------'
+}
 
 #--- copy the new stuff to the Pico ---
+copy_release_files_to_the_pico() {
+    echo 'Copy contents of dir  anr_http'
+    mpremote fs cp -r anr_http :
+    
+    echo 'Copy anr_http_client.py'
+    mpremote fs cp anr_http_client/anr_http_client.py :/
+}
 
-echo 'Copy contents of dir  anr_http'
-mpremote fs cp -r anr_http :
+list_pico_filesystem_contents() {
+    echo ' '
+    echo 'LIST THE PICO CONTENTS:'
+    mpremote fs ls 
+    
+    mpremote fs ls /anr_http
+}
 
-echo 'Copy anr_http_client.py'
-mpremote fs cp anr_http_client/anr_http_client.py :/
+main() {
+    echo "MAIN"
+    clean_up_old_dirs_and_files
+    copy_release_files_to_the_pico
+    list_pico_filesystem_contents
+}
 
-echo ' '
-echo 'LIST THE PICO CONTENTS:'
-mpremote fs ls 
-
-mpremote fs ls /anr_http
+main $*
 
 
 ###
