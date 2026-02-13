36b7123eab785e8b005eece2aefaa51f325c080a
Art SHADOW <art@SHADOW>
2026-02-12 23:22:27 -0600

    WIP simple async server can receive request from firefox and sends back a reply with no HTML content

--- pi_pico/examples/asyncio_examples/simple_async_server.py
+++ pi_pico/examples/asyncio_examples/simple_async_server.py
@@ -7,6 +7,7 @@
 import asyncio
 import machine
 import network
+import sys
 import urequests
 import utime
 
@@ -67,7 +68,114 @@ def connect_to_wifi(show_details=True):
     return wlan, ip_addr
 
 
-async def handle_client(reader, writer):
+async def handle_client_by_lines(reader, writer):
+    """
+    Coroutine to handle a single client connection.
+    Takes StreamReader and StreamWriter instances as arguments.
+    """
+    print('New client connected...')
+    try:
+        lno = 0
+        lines = []
+        while True:
+            # Read line by line from the client
+            print(f"handle_client_by_lines@82  ======  READ A LINE  =================================")
+            new_bytes = await reader.readline()
+            if not new_bytes:
+                # Client disconnected
+                print(f"handle_client_by_lines@86 GOT NO MORE BYTES, client disconnected")
+                break
+            lno += 1
+            print(f"handle_client_by_lines@89 {lno=} got {len(new_bytes)} bytes. ")
+            
+            # print the last 2 bytes of each line
+            s = ""
+            if len(new_bytes) >= 2:
+                s += f"  2nd-last-byte is {int(new_bytes[-2])} "
+            if len(new_bytes) >=1:
+                s += f"  last-byte is {int(new_bytes[-1])} "
+            if s:
+                print(f" {lno=} {s}")
+                
+            # last 2 bytes are 13 10  ie \r\n
+            ###if len(new_bytes) == 2:
+            ###    print()
+            ###    print(f"handle_client_by_lines@103  2 bytes are {int(new_bytes[0])}  {int(new_bytes[1])} ")
+            
+            new_line = new_bytes.decode('utf8').strip()
+            print(f'Received: {lno=}. {new_line}')
+
+            ###if message.lower() == 'quit':
+            ###    break
+            lines.append(new_line)
+            print(f"  there are now {len(lines)} lines received.")
+
+            # are the last 2 bytes  13 10  ie \r\n
+            if len(new_bytes) == 2:
+                print()
+                print(f"handle_client_by_lines@116  2 bytes are {int(new_bytes[0])}  {int(new_bytes[1])} ")
+                if int(new_bytes[0]) == 13 and int(new_bytes[1]) == 10:
+                    print(f"  THIS IS THE LAST LINE!!!!!!!!!!! STOP READING!!!!!!!!!")
+                break
+        if 1:
+            if lines[0][0:3] == "GET":
+                print(f"  REPLACE GET with 200 OK")
+                lines[0] = "HTTP/1.1 200 OK\r\n"
+            message = "".join(lines)
+            ###message += "\r\n\r\n"
+            # Send the response back to the client
+            mesg_bytes = message.encode("utf-8")
+            print(f" Write the mesg.  len(mesg_bytes) = {len(mesg_bytes)}")
+            ###writer.write(f'Echo: {message}\n'.encode('utf8'))
+            writer.write(mesg_bytes)
+            # add trailing blank line
+            ####writer.write(f"\r\n".encode("utf-8"))  ### ANR
+            await writer.drain() # Ensure the data is sent
+            
+    except Exception as e:
+        print(f'Error with client: {e}')
+    finally:
+        print('Closing connection')
+        writer.close()
+        await writer.wait_closed() # Wait until the stream is fully closed
+
+async def handle_client_as_bytes(reader, writer):
+    """
+    Coroutine to handle a single client connection.
+    Takes StreamReader and StreamWriter instances as arguments.
+    """
+    print('New client connected...')
+    try:
+        all_bytes = bytearray(b"")
+        while True:
+            # Read line by line from the client
+            new_bytes = await reader.readline()
+            if not new_bytes:
+                # Client disconnected
+                print(f"handle_client_by_lines GOT NO MORE BYTES, client disconnected")
+                break
+            print(f"handle_client_by_lines@122  got {len(new_bytes)} bytes.  len(all)={len(all_bytes)}")
+            
+            all_bytes.extend(new_bytes)
+            print(f"handle_client_by_lines@125  got {len(new_bytes)} bytes.  len(all)={len(all_bytes)}")
+            ###message = line.decode('utf8').strip()
+            ###print(f'Received: {message}')
+
+            ###if message.lower() == 'quit':
+            ###    break
+
+            # Send the response back to the client
+            ###writer.write(f'Echo: {message}\n'.encode('utf8'))
+            ###await writer.drain() # Ensure the data is sent
+            
+    except Exception as e:
+        print(f'Error with client: {e}')
+    finally:
+        print('Closing connection')
+        writer.close()
+        await writer.wait_closed() # Wait until the stream is fully closed
+
+async def handle_client_all_bytes(reader, writer):
     """
     Coroutine to handle a single client connection.
     Takes StreamReader and StreamWriter instances as arguments.
@@ -75,12 +183,17 @@ async def handle_client(reader, writer):
     print('New client connected...')
     try:
         while True:
-            # Read line by line from the client
-            line = await reader.readline()
-            if not line:
-                break  # Client disconnected
-            
-            message = line.decode('utf8').strip()
+            # this stalls until the client/browser is told to give up on the request (press X on browser gui)
+            print(f"handle_client_all_bytes read everything!")
+            got_bytes = await reader.read(-1)
+
+            print(f"handle_client_all_bytes@112 got {None if got_bytes is None else len(got_bytes)} ")
+            if got_bytes is None or len(got_bytes) <= 0:
+                print(f"handle_client_all_bytes - got nothing!")
+                break
+            print(f" @@@@ 116")
+            message = got_bytes.decode('utf8').strip()
+            print(f'Received: decode.len={len(message)}')
             print(f'Received: {message}')
 
             if message.lower() == 'quit':
@@ -102,7 +215,9 @@ async def run_server(host, port):
     Starts the asynchronous server.
     """
     # asyncio.start_server returns a Server object (or a generator in older MicroPython versions)
-    server = await asyncio.start_server(handle_client, host, port)
+    ###callbk = handle_client_all_bytes
+    callbk = handle_client_by_lines
+    server = await asyncio.start_server(callbk, host, port)
     print(f'Listening on {host}:{port}...')
     print(f"Server obj is {type(server)}")
     print(f"server module {server.__module__}")
