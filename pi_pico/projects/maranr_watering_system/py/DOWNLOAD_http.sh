dfa2a704f2b0881d4ea05b5e7a41733548d9185d
Art SHADOW <art@SHADOW>
2026-03-01 15:02:14 -0600

    MWS diff scripts

---
+++ pi_pico/projects/maranr_watering_system/py/DOWNLOAD_http.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# DOWNLOAD_http.sh
+
+DBG=false
+
+for fp in http/* ; do
+
+    if $DBG ; then  echo ff is $fp ; fi
+    fn=$(basename $fp)
+    if $DBG ; then echo fn is $fn ; fi
+
+    if $DBG ; then echo \
+    "diff -q http/$fn  /tmp/HTTP/$fn" ; fi
+    diff  -q http/$fn  /tmp/HTTP/$fn
+    result="$?"
+    if [ "$result" == 0 ] ; then
+        if $DBG ; then echo matches ; fi
+    fi
+    if [ "$result" == 1 ] ; then
+        if $DBG ; then echo "NOT matches  http/$fn  /tmp/HTTP/$fn" ; fi
+        echo \
+        "cp -p tmp/HTTP/$fn  http/$fn  "
+        cp  -p tmp/HTTP/$fn  http/$fn
+    fi
+
+done
+
+
+#cp -p http/* /tmp/HTTP/
+
+###
