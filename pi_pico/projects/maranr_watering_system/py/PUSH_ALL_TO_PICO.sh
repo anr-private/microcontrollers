41e1fa95bccc3674d94bf9bb46586001b9e4d951
Art SHADOW <art@SHADOW>
2026-03-04 16:21:21 -0600

    Pico MWS: scripts

--- pi_pico/projects/maranr_watering_system/py/PUSH_ALL_TO_PICO.sh
+++ pi_pico/projects/maranr_watering_system/py/PUSH_ALL_TO_PICO.sh
@@ -1,18 +1,28 @@
 #!/bin/sh
 #
+# PUSH_ALL_TO_PICO.sh
+#
 # copy all files to the pico
 
+rm -rf displays/__pycache__
 rm -rf http/__pycache__
+rm -rf lib/__pycache__
+rm -rf primitives/__pycache__
+rm -rf sensors/__pycache__
+rm -rf trivlog/__pycache__
 
-#--- get rid of the old stuff ---
-clean_up_old_dirs_and_files() {
-    echo 'CLEANUP: copy the cleanup program and run it'
-    mpremote fs cp remove_old_watering_files.py  :/
-    echo '   Run the cleanup'
-    mpremote run remove_old_watering_files.py
-    echo 'CLEANUP is done'
-    echo '----------------'
-}
+# USE  'REMOVE_ALL_from_pico.sh'
+###
+####--- get rid of the old stuff ---
+###clean_up_old_dirs_and_files() {
+###    echo 'CLEANUP: copy the cleanup program and run it'
+###    mpremote fs cp remove_old_watering_files.py  :/
+###    echo '   Run the cleanup'
+###    mpremote run remove_old_watering_files.py
+###    echo 'CLEANUP is done'
+###    echo '----------------'
+###}
+
 
 #--- copy the new stuff to the Pico ---
 copy_release_files_to_the_pico() {
@@ -42,10 +52,10 @@ list_pico_filesystem_contents() {
 }
 
 main() {
-    echo "MAIN"
-    clean_up_old_dirs_and_files
-#    copy_release_files_to_the_pico
-#    list_pico_filesystem_contents
+    echo "PUSH_ALL_TO_PICO.sh  MAIN-PGM"
+    ###clean_up_old_dirs_and_files
+    copy_release_files_to_the_pico
+    list_pico_filesystem_contents
 }
 
 main $*
--- pi_pico/projects/maranr_watering_system/py/UPLOAD_ALL.sh
+++ pi_pico/projects/maranr_watering_system/py/UPLOAD_ALL.sh
@@ -1,6 +1,7 @@
 # UPLOAD_ALL.sh
 #
 ./UPLOAD_main.sh
+./UPLOAD_http.sh
 ./UPLOAD_lib.sh
 ##./UPLOAD_logger.sh
 ./UPLOAD_http.sh
