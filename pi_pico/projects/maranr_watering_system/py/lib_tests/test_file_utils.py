a17f6d3fe8452788bce79c2a6267e5acee391985
Art SHADOW <art@SHADOW>
2026-03-10 16:24:16 -0500

    MWS: lib_tests updated for FileUtils that reads N lines from tail of a file

--- pi_pico/projects/maranr_watering_system/py/lib_tests/test_file_utils.py
+++ pi_pico/projects/maranr_watering_system/py/lib_tests/test_file_utils.py
@@ -20,14 +20,32 @@ def create_a_text_file(fpath):
 
 
 def test1():
-    fpath = "junk_test.txt"
+    fpath = "file_20_lines.txt"
     create_a_text_file(fpath)
 
     fu = FileUtils()
 
-    # Read the last 5 lines
-    last_lines = fu.read_last_n_lines(fpath, 5)
+    
+    print(f"\nTest 1: Read the last 5 lines")
+    print("  Expect lines 16..20")
+    last_lines = fu.read_last_n_lines(fpath, 5, 999)
+    print("Last 5 lines:")
+    for line in last_lines:
+        print(line.strip())
 
+    print(f"\nTest 2: Read 5 lines, start 10 lines from the end of file")
+    print(f"  Expect lines 10-14")    
+    # Read the last 5 lines starting at 10 lines from the end
+    last_lines = fu.read_last_n_lines(fpath, 10, 5)
+    print("Last 5 lines:")
+    for line in last_lines:
+        print(line.strip())
+
+
+    print(f"\nTest 3: Read 1 line, start 12 lines from the end of file")
+    print(f"  Expect line 8")    
+    # Read the last 5 lines starting at 10 lines from the end
+    last_lines = fu.read_last_n_lines(fpath, 12, 1)
     print("Last 5 lines:")
     for line in last_lines:
         print(line.strip())
