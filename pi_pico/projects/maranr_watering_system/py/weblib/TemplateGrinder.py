2885a054b9736cae3965b7869b1b953576ecbbc9
Art SHADOW <art@SHADOW>
2026-03-14 22:56:52 -0500

    MWS TemplateGrinder  use dict of function pointers instead of just str

--- pi_pico/projects/maranr_watering_system/py/weblib/TemplateGrinder.py
+++ pi_pico/projects/maranr_watering_system/py/weblib/TemplateGrinder.py
@@ -20,6 +20,15 @@ START_MARKER = "[["
 END_MARKER = "]]"
 
 
+BODY_SUBS = {
+    # NOTE keys are always all lower case!
+    "filesystem-status-splitline": (get_fs_space_string, (), {"sep":"<br>"} ),
+    "memory-status": (get_memory_status_string, ()),
+    "date-time": (get_formatted_date_time_string, ()),
+    }
+
+
+
 class TemplateGrinder(ElemLoggerABC):
     
     def __init__(self):
@@ -33,7 +42,24 @@ class TemplateGrinder(ElemLoggerABC):
         logrt = logger.logrt
         logi = logger.logi
 
-    def fetch_value_for_symbol(self, symbol_stg):
+    def _fetch_value_for_symbol(self, symbol_stg):
+        funct_info = BODY_SUBS.get(symbol_stg)
+        if funct_info is None:
+            return f"UNKOWN:{symbol_stg}"
+            
+        funct = funct_info[0]
+        args = ()
+        kw = {}
+        if len(funct_info) > 1:
+            args = funct_info[1]
+        if len(funct_info) > 2:
+            kw = funct_info[2]
+
+        r = funct(*args, **kw)
+        return r
+
+
+    def OLD__fetch_value_for_symbol(self, symbol_stg):
         if symbol_stg.lower() == "filesystem-status-splitline":
             return get_fs_space_string(sep="<br>")
         if symbol_stg.lower() == "memory-status":
@@ -48,32 +74,6 @@ class TemplateGrinder(ElemLoggerABC):
         #print(f"TGND@38 @@@@@@@@@@ _split_the_file_contents  {type(lines)=}")
         #print(f"TGND@38 @@@@@@@@@@ _split_the_file_contents  {type(lines[0])=}")
         return lines
-
-    def process_the_lines(self, lines):
-
-        processed_lines = list()
-
-        for lno, line in enumerate(lines):
-
-            #if 0: print(f"@@@@ {lno} {line}")
-
-            processed_line = self.process_one_line(lno, line)
-
-            processed_lines.append(processed_line)
-
-        return processed_lines
-
-
-    def process_one_line(self, lno, raw_line):
-
-        processed_line = raw_line
-        while 1:
-            updated = self.substitute_into_line(lno, processed_line)
-            if updated is None:
-                break
-            processed_line = updated
-        return processed_line
-
 
     def substitute_into_line(self, lno, processed_line):
         start_pos = processed_line.find(START_MARKER, 0)
@@ -100,27 +100,56 @@ class TemplateGrinder(ElemLoggerABC):
         #print(f"@@61 found line {lno}. {processed_line}")
         #print(f"@@64 {len(headb)=}  {headb=}    {len(tailb)=} {tailb=}")
 
-        value = self.fetch_value_for_symbol(symbol)
+        value = self._fetch_value_for_symbol(symbol)
 
         processed_line = "".join([headb, value, tailb])
         #print(f"@@84  {processed_line=}")
         return processed_line
 
 
+    def grind_one_line(self, lno, raw_line):
+
+        processed_line = raw_line
+        # repeat substitution - handles nested substitution items like [[my [[date]] bday]]
+        while 1:
+            updated = self.substitute_into_line(lno, processed_line)
+            if updated is None:
+                break
+            processed_line = updated
+        return processed_line
+
+
+    def grind_lines(self, lines):
+
+        processed_lines = list()
+
+        for lno, line in enumerate(lines):
+
+            #if 0: print(f"@@@@ {lno} {line}")
+
+            processed_line = self.grind_one_line(lno, line)
+
+            processed_lines.append(processed_line)
+
+        return processed_lines
+
+
     def grind_file_contents(self, raw_file_contents):
-        ###@@@@@@@@@@@@@@@
+        # raw_file_contents is a str containing an entire file or
+        # a chunk of file. This method breaks it into a list of lines
+        # and then grinds the lines.
 
         if isinstance(raw_file_contents, str):
             file_contents = raw_file_contents
         elif isinstance(raw_file_contents, bytes):
             file_contents = raw_file_contents.decode("utf-8")
         else:
-            logi("RH@109 @@@@@@@@@@@@@@@@@@ UNEXPECTED FILE CONTENTS TYPE={type(raw_file_contents)}")
-            return file_contents
+            logi(f"RH@109 @@@@@@@@@@@@@@@@@@ UNEXPECTED FILE CONTENTS TYPE: {type(raw_file_contents)}")
+            return None
 
         raw_lines = self._split_the_file_contents(file_contents)
 
-        processed_lines = self.process_the_lines(raw_lines)
+        processed_lines = self.grind_lines(raw_lines)
 
         ###new_contents_by tes = b"\n".join(processed_lines)
         new_contents_stg = "\n".join(processed_lines)
