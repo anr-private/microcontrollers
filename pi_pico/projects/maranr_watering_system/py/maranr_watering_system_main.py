4d174c58a79afb84a64df694048206066b2431a0
Art SHADOW <art@SHADOW>
2026-03-18 13:09:01 -0500

    MWS: copied MaranrWateringSystem.py and main from pico/examples/asyncio_wifi/ - uses updated MwsWifi so now handles connect/disconnect and requires MWS code to be modified to match the new wifi approach. Mainly web server should now deal with wifi disconnect/reconnect

--- pi_pico/projects/maranr_watering_system/py/maranr_watering_system_main.py
+++ pi_pico/projects/maranr_watering_system/py/maranr_watering_system_main.py
@@ -1,9 +1,6 @@
 # maranr_watering_system_main.py
 #
-# main program for the MARANR watering system (MWS) project
-#
-# Gets install on the Pico as  /main.py
-# Runs when Pico powers up.
+
 
 import asyncio
 import sys
@@ -17,10 +14,10 @@ except Exception:
 
 from logger_elem.ElemLoggerABC import ElemLoggerABC, ElemLogControl
 
-from displays.MwsDisplays import MwsDisplays
-from sensors.MwsSensors import MwsSensors
+#from displays.MwsDisplays import MwsDisplays
+#from sensors.MwsSensors import MwsSensors
 from lib2.MwsWifi import MwsWifi
-from weblib.MwsWebServer import MwsWebServer
+#from weblib.MwsWebServer import MwsWebServer
 from utils import MWS_CONFIG
 from utils import get_formatted_local_time
 from utils import get_fs_space_string
@@ -42,129 +39,8 @@ logi = None
 print(f"@@@MAIN@42  {MWS_CONFIG=}")
 
 
-class MaranrWateringSystem(ElemLoggerABC):
 
-    def __init__(self):
-        super().__init__()
-
-    def _set_logger(self, logger):
-        global log, logrt, logi
-        print(f"MAIN@52 _set_logger: {repr(logger)}")
-        print(f"MAIN@53 _set_logger: {str(logger)}")
-        log = logger.log
-        logrt = logger.logrt
-        logi = logger.logi
-
-
-    async def main_task(self, host, port):
-    
-        webserver = MwsWebServer(host, port) 
-        webserver_task = webserver.start_the_task()
-        log(f"MWSMAIN@78 {webserver_task=}")
-    
-        sensors = MwsSensors()
-        sensors_task = sensors.start_the_task()
-        log(f"MWSMAIN@82 {sensors_task=}")
-        
-        displays = MwsDisplays()
-        displays_task = displays.start_the_task()
-        log(f"MWSMAIN@86 {displays_task=}")
-        
-        while 1:
-            log(f"MWSMAIN@89  MAIN TASK running    {host=}   {port=} ")
-            webserver_done = webserver_task.done()
-            sensors_done = sensors_task.done()
-            displays_done = displays_task.done()
-    
-#MAIN@96 FS: TOTAL SPACE 868,352 bytes, 848.00 KB, 0.83 MB   FREE SPACE 507,904 bytes, 496.00 KB, 0.48 MB
-#MAIN@97 MEMORY:  gc.mem_alloc()=90240   gc.mem_free()=115328
-#MAIN@99 MEMORY AFTER GC:  gc.mem_alloc()=64464   gc.mem_free()=141104
-
-            log(f"  Who is done:  web={webserver_task.done()}  "+\
-                   f"displays={displays_task.done()}  sensors={sensors_task.done()}")
-            print(f"MAIN@96 FS: {get_fs_space_string()}")
-            print(f"MAIN@97 MEMORY: {get_memory_status_string(do_garbage_collect=False)}")
-            gc.collect()
-            print(f"MAIN@99 MEMORY AFTER GC: {get_memory_status_string(do_garbage_collect=False)}")
-
-            #log(f"   state: {sensors_task.state}")  # bool
-            #log(f"   data: {sensors_task.data}")    # None
-            
-            # not impl in micropython
-            ###done, pending = await asyncio.wait(tasks, timeout=1)
-    
-            if webserver_done and sensors_done and displays_done:
-                log("MWSMAIN@59  MAIN_TASK: all tasks are done!")
-                break
-            
-            await asyncio.sleep(11)
-            
-        if 0:   # NOT IMPL
-            log(f"MWSMAIN@114 webserver result={webserver_task.result()}")
-            log(f"MWSMAIN@115 sensors   result={sensors_task.result()}")
-            log(f"MWSMAIN@116 displays  result={displays_task.result()}")
-        
-    
-    def connect_to_wifi(self):
-
-        mws_wifi = MwsWifi.get_instance()
-
-        wlan, ip_addr = mws_wifi.connect_to_wifi()
-    
-    
-        MAX_RETRIES = 10
-        num_retries = 0
-        while num_retries < MAX_RETRIES:
-            ok = mws_wifi.wifi_set_time_from_ntp(wlan)
-            ###print(f"MAIN@130  set time returnned {ok=}")
-            if ok:
-                m = "MWSMAIN@132  SUCCESSFULLY UPDATED SYSTEM TIME from NTP"
-                logi(m)
-                break
-            num_retries += 1
-            m = "MWSMAIN@86  **ERROR** FAILED TO UPDATE SYSTEM TIME from NTP. {num_retries=}"
-            time.sleep(1)
-    
-        date_stg, time_stg = get_formatted_local_time()
-        m = f"MWSMAIN@141 MAIN  CONNECTED TO WIFI.  local date,time: {date_stg}  {time_stg} "
-        logi(m)
-        m = f"MWSMAIN@144 MAIN  CONNECTED TO WIFI.  {ip_addr=}  wlan={wlan}"
-        logi(m)
-    
-        # You will likely need to replace '0.0.0.0' with your device's actual IP address
-        # after connecting it to a network.
-        # '0.0.0.0' makes the server listen on all available network interfaces.
-        ### ANR host = '0.0.0.0'
-        host = ip_addr
-        port = 8000
-    
-        return host,port
-
-    def run_mws(self):
-
-        #@@@@@_log_start()
-
-        logi("--- MWS --- BEGIN run_mws()  =======================")
-
-        print(f"MAIN@149 FS: {get_fs_space_string()}")
-        print(f"MAIN@150 MEMORY: {get_memory_status_string(do_garbage_collect=False)}")
-        print(f"MAIN@151  +++++ DO GC COLLECT   ++++++++++++++++++")
-        gc.collect()
-        print(f"MAIN@153 MEMORY AFTER GC: {get_memory_status_string(do_garbage_collect=False)}")
-
-        host,port = self.connect_to_wifi()
-
-        try:
-            # Start the event loop and run the main server coroutine
-            asyncio.run(self.main_task(host, port))
-        except KeyboardInterrupt:
-            date_stg, time_stg = get_formatted_local_time()
-            m = f"MWSMAIN@162 {date_stg} {time_stg}  Server stopped by user KeyboardInterrupt."
-            log(m)
-            logi(m)
-        finally:
-            # Clean up the event loop (optional, but good practice)
-            asyncio.new_event_loop()
+from MaranrWateringSystem import MaranrWateringSystem
 
 
 def main():
@@ -185,4 +61,3 @@ if __name__ == '__main__':
     main()
     
 ### end ###
-
