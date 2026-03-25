389bae02842c7e542eeaae122649edea3bcd0a92
Art SHADOW <art@SHADOW>
2026-03-24 19:44:37 -0500

    MWS - log settings page creates checkboxes for classes - checkbox handler outputs to console but does not send to server yet

--- pi_pico/projects/maranr_watering_system/pages/fetch_data_async.js
+++ pi_pico/projects/maranr_watering_system/pages/fetch_data_async.js
@@ -8,7 +8,7 @@ async function fetchDataAsync(url, callbk) {
     console.log("fetchDataAsync@8  isFetching=" + isFetching);
 
     if (isFetching) {
-        console.log("fetchDataAsync@8 FETCHING IS ALREADY ACTIVE!");
+        console.log("fetchDataAsync@11 FETCHING IS ALREADY ACTIVE!");
         return;
     }
     isFetching = true;
@@ -16,41 +16,37 @@ async function fetchDataAsync(url, callbk) {
     try {
         const response = await fetch(url);
 
-        console.log("fetchDataAsync@9 got response:..."); console.log(response);
+        console.log("fetchDataAsync@19 got response:..."); console.log(response);
 
         if ( ! response.ok) {
             throw new Error(`HTTP error: ${response.status}`);
         }
 
-        console.log("fetchDataAsync@16 AWAIT response:json");
+        console.log("fetchDataAsync@25 AWAIT response:json");
         
-        const data1 = await response.json();
+        const data = await response.json();
 
-        console.log("fetchDataAsync@20  data1:..."); 
-        console.log(data1);
+        console.log("fetchDataAsync@29  data:..."); 
+        console.log(data);
 
-        //const data2 = await data1;
-        const data2 = data1;
+        callbk(data);
 
-        // Use the 'data' here
-        console.log("fetchDataAsync@27  data2:..."); 
-        console.log(data2);
-        callbk(data2);
+        console.log("fetchDataAsync@34 ALL DONE @@@@@@@@@@_______________@@@@@@@@@@@@@@@@@______________@@@@@@@@@@@");
 
-        console.log("fetchDataAsync@31 ALL DONE @@@@@@@@@@_______________@@@@@@@@@@@@@@@@@______________@@@@@@@@@@@");
-
-        //@@@@return data2;
+        // Cannot return the data like this - because this function 
+        // is async, the data gets wrapped in a Promise and the
+        // caller must be async to unwrap it.
+        ////return data
 
       } catch (error) {
           // Handle any errors that occurred during the fetch or processing
-          console.error('fetchDataAsync@26 Error fetching data:', error);
+          console.error('fetchDataAsync@43 Error fetching data:', error);
           throw error; // Re-throw if necessary
       } finally {
-          console.log("fetchDataAsync@49 FETCH COMPLETED.  RESETTING THE FLAG");
+          console.log("fetchDataAsync@46 FETCH COMPLETED.  RESETTING THE FLAG");
           isFetching = false;
       }
 }
 
-// How to use the async function
-// data = fetchData('https://api.example.com/data');
+// ####
 
