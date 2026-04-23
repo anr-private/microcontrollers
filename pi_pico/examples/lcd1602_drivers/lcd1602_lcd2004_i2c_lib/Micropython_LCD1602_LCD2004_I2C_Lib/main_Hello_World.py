063a2b8c570dde6ef3e20a0760ed87c8b185f841
Art SHADOW <art@SHADOW>
2026-04-22 19:29:48 -0500

    Pico: notes on using LCD 1602 and level shifter so LCD can run at 5v while Pico GPIO are 3.3v

--- pi_pico/examples/lcd1602_drivers/lcd1602_lcd2004_i2c_lib/Micropython_LCD1602_LCD2004_I2C_Lib/main_Hello_World.py
+++ pi_pico/examples/lcd1602_drivers/lcd1602_lcd2004_i2c_lib/Micropython_LCD1602_LCD2004_I2C_Lib/main_Hello_World.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-__author__ = 'LiYuanhe'
+###__author__ = 'LiYuanhe'
 
+import time
 from machine import Pin, SoftI2C
 from lib_lcd1602_2004_with_i2c import LCD
 #scl_pin = 26
@@ -11,7 +12,13 @@ sda_pin = 2
 scl_pin = 3
 
 lcd = LCD(SoftI2C(scl=Pin(scl_pin), sda=Pin(sda_pin), freq=100000))
-lcd.puts("Hello, World!")
-lcd.puts("Hello World row2", x=0, y=1)
 
+ctr = 0
+while 1:
+    print(f"  ctr={ctr}")
+    lcd.puts(f"Hello, World! {ctr}")
+    lcd.puts("Hello World row2", x=0, y=1)
+    ctr += 1
+    time.sleep(1)
+    
 ###
